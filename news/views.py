aab6e7c2c1bf685e46334a3dfa46f9e5beda997f
Oleh Karmazin <kalemzwork@gmail.com>
2025-07-29 13:35:46 +0200

    Update and delete news

--- news/views.py
+++ news/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from news.models import Articles
 from news.forms import ArticlesForm
-from django.views.generic import DetailView
+from django.views.generic import DetailView, UpdateView, DeleteView
 
 def news_home(request):
     news = Articles.objects.order_by('-date')
@@ -11,6 +11,18 @@ class NewsDetailView(DetailView):
     model = Articles
     template_name = 'news/details_view.html'
     context_object_name = 'article'
+
+class NewsUpdateView(UpdateView):
+    model = Articles
+    template_name = 'news/create.html'
+
+    form_class = ArticlesForm
+
+
+class NewsDeleteView(DeleteView):
+    model = Articles
+    success_url = '/news/'
+    template_name = 'news/news_delete.html'
 
 def create(request):
     error = ''
@@ -30,3 +42,4 @@ def create(request):
     }
 
     return render(request, 'news/create.html', data)
+
